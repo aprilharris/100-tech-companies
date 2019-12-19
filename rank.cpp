4dd47786ab4ace5ba7087b82fcb6c6fa2026471e
aprilharris <april@aprilharris.co>
2019-12-19 16:31:46 -0500

    inish commit

---
+++ rank.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+char n='#';
+char c = '#';
+
+for (int i=1; i<=100; i++) {
+cout << endl<<endl<<"{\nrank: "<<i<<"\nstartup:\"#\",\nfounded: \"#\",\nemployees:\"#\",\nlocation: \"#\",\nindustry: " << 1 <<",\nurl: \"#\"\n}," << endl;
+
+}
+
+
+
+
+ return 0;
+}
